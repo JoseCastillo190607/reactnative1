7e14327658b6d4e932bf9aec96d845af84e20925
desarrollo <jose.castillosanchezits@gmail.com>
2022-09-13 23:40:47 -0500

    feact: add label with my info

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text style={styles.Text}>Jose Guadalupe Castillo</Text> */}
       <StatusBar style="auto" />
+      <Text style={styles.Text}>
+      </Text>
+      <Text>
+        <Text style={styles.Text}>
+        <b>Jose Gpe Castillo Sanchez</b> <br/>
+           <b style={styles.Text}>               190607</b>
+        </Text>
+        <StatusBar style="auto" />
+      </Text>
     </View>
   );
 }
@@ -13,8 +22,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    // backgroundImage: URL(),
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom:100,
   },
+
+  Text: {
+    color: "#fff",
+    fontSize: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "25%",
+    height: "55%",
+    borderRadius: "50%",
+    backgroundColor: "red",
+    marginBottom:-200,
+    
+  },
+
+  texto1:{
+    marginBottom:500
+  }
 });
